7035e90241db0ee16a6523b50dfc2fd278653534
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-01-12 22:20:00 +0530

    Update EvenOdd.c

--- EvenOdd.c
+++ EvenOdd.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
     int a;
-    printf("Enter number:");
-    scanf ("%d",&a);
-    if (a%2 == 0){
-        printf("Even");
+    printf("Enter the number:");
+    scanf ("%d", &a);
+    if (a % 2 == 0){
+        printf("It is Even");
     }
     else
     {
-        printf("Odd");
+        printf("It is Odd");
     }
     return 0;
 }
