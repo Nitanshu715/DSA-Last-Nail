c358fa9215c3eec2666cf678ff4fd9b188e4afa9
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-02-07 01:41:19 +0530

    Create InputSquare.cpp

---
+++ InputSquare.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the number you want to square of: ";
+    cin>>x;
+    cout<<"The Square of the number is: ";
+    cout<<x*x;
+    
+}
