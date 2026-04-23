d63410d882ac89a1bb7185d8dfb38dd45c1543a6
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-04-23 11:14:46 +0530

    Create getline.cpp

---
+++ practice/getline.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    string name;
+    cout << "tell your age and name!\n";
+    cin >> age;
+    cin.ignore();
+    getline(cin, name);
+    cout << "Hello " << name << " You are " << age << " Years old!";
+    return 0;
+}
