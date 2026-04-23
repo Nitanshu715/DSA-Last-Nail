e2988193ba64a649ba586c99a3b42510d9888222
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-04-23 11:35:38 +0530

    Update Conditional.cpp

--- practice/Conditional.cpp
+++ practice/Conditional.cpp
@@ -9,12 +9,18 @@ int main()
     cin.ignore();
     cout << "Enter your name: ";
     getline(cin, name);
-    if (age >= 18)
+    if (age > 18)
     {
         cout << "Hello " << name << ", you are an adult!";
     }
-    else {
+    if (age == 18)
+    {
+        cout << "Last phase of teenage!";
+    }
+    else 
+    {
         cout << "Hello " << name << ", you are a minor!";
     }
+    
     return 0;
 }
