2689a54b4c11ac656d7cb10f29a69610c5be64f5
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-04-23 14:53:20 +0530

    Create FindZeroin2D.cpp

---
+++ practice/FindZeroin2D.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, arr[3][3];
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            cout << "Enter the number: ";
+            cin >> arr[i][j];
+        }
+    }
+    for (i = 0; i < 3; i++)
+    {
+        cout << endl;
+        for (j = 0; j < 3; j++)
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+    cout << endl;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (arr[i][j] == 0)
+            {
+               cout << "The row: " << i << " contains a 0.";
+               cout << "The column: " << j << " contains a 0."; 
+            }
+        }
+    }
+    return 0;
+}
